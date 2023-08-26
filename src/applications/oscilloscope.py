865f0ae3f82a4ad388c567515a76c40cb9577318
vasilisniaouris <vasilisniaouris@gmail.com>
2023-08-26 14:20:43 -0700

    Updated `oscilloscope.py` to display rolling mean line and reading text.

--- src/applications/oscilloscope.py
+++ src/applications/oscilloscope.py
@@ -14,8 +14,9 @@ from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg, NavigationToolb
 
 import nidaqmx
 
-import qt3utils.nidaq
-import qt3utils.datagenerators as datasources
+import src.qt3utils.nidaq
+import src.qt3utils.datagenerators as datasources
+from src.qt3utils.math_utils import get_rolling_mean
 
 logger = logging.getLogger(__name__)
 
@@ -52,10 +53,20 @@ parser.add_argument('-sc', '--signal-counter', metavar='ctrN', default='ctr2', t
                     help='NI DAQ interal counter (ctr1, ctr2, ctr3, ctr4)')
 parser.add_argument('-r', '--randomtest', action='store_true',
                     help='When true, program will run showing random numbers. This is for development testing.')
-parser.add_argument('-aut', '--animation-update-interval', metavar='milliseconds', default=20,
+parser.add_argument('-aut', '--animation-update-interval', metavar='milliseconds', default=20, type=float,
                     help='''Sets the animation update period, t, (in milliseconds).
                     This is the time delay between calls to acquire new data.
                     You should be limited by the data acquisition time = N / clock_rate.''')
+parser.add_argument('-rmw', '--rolling-mean-window', metavar='milliseconds', default=400, type=float,
+                    help='''Sets the displayed rolling mean window size, w, (in milliseconds).
+                    Actual value will set as the ceil(rmw/aut) * aut.''')
+parser.add_argument('-srm', '--show-rolling-mean', default='Υ',
+                    help='''Show (Y) or hide (N) the rolling mean line.''')
+parser.add_argument('-srt', '--show-reading-text', default='Υ',
+                    help='''Show (Y) or hide (N) the reading text. 
+                    If rolling mean is enabled, rolling mean, rolling standard
+                    deviation (STD) and expected STD (from Poison distribution) 
+                    text will also be displayed.''')
 
 parser.add_argument('--console', action='store_true',
                     help='Run as console app -- just the figure without buttons.')
@@ -64,7 +75,11 @@ args = parser.parse_args()
 
 class ScopeFigure:
 
-    def __init__(self, width=50, fig=None, ax=None):
+    def __init__(self, width: int = 50, rolling_mean_window_size: int = 20,
+                 show_rolling_mean: bool = True, show_reading_text: bool = True,
+                 fig: plt.Figure = None, ax: plt.Axes = None):
+
+        # setting up the figure and main axis
         if ax is None:
             fig, ax = plt.subplots()
             self.fig = fig
@@ -72,14 +87,58 @@ class ScopeFigure:
         else:
             self.fig = fig
             self.ax = ax
+        self.fig.set_layout_engine('compressed')
 
+        # display settings
+        self.show_rolling_mean = show_rolling_mean
+        self.show_reading_text = show_reading_text
+
+        # setting up primary data plot
         self.ydata = collections.deque(np.zeros(width))
         self.line, = self.ax.plot(self.ydata)
-        self.ax.set_ylabel('counts / sec')
+        self.line: plt.Line2D
+
+        # Setting up axis preferences
+        # TODO: Put all axis preferences to .mplstyle and load through terminal or gui.
+        self.ax.set_ylabel('Reading (counts / sec)')
         self.ax.ticklabel_format(style='sci', scilimits=(-3, 4), axis='y')
+        self.ax.set_xlabel('Acquisition Number')
+
+        if self.show_rolling_mean:
+            self._add_rolling_mean(rolling_mean_window_size)
+        if self.show_reading_text:
+            self._add_reading_text()
+
+    def _add_rolling_mean(self, rolling_mean_window_size: int):
+        self.rolling_mean_window_size = rolling_mean_window_size
+        self.half_window_size = int(np.ceil(self.rolling_mean_window_size / 2))
+        self.rolling_mean = self.get_rolling_mean()
+        self.rolling_mean_line, = self.ax.plot(self.rolling_mean)
+        self.rolling_mean_line: plt.Line2D
+
+    def _add_reading_text(self):
+        self.text_font_size = 30  # TODO: Add to .mplstyle file
+        fig_width = self.fig.get_size_inches()[0]
+        fig_height = self.fig.get_size_inches()[1]
+        if self.show_rolling_mean:
+            self.fig.set_size_inches(fig_width, fig_height * 1.3)
+            displayed_values = 'NaN\nNaN\nNaN (NaN)'
+            displayed_labels = 'Cur. Val.\nMean\nSTD'
+        else:
+            self.fig.set_size_inches(fig_width, fig_height * 1.1)
+            displayed_values = 'NaN'
+            displayed_labels = 'Cur. Val.'
+        self.current_value_text: plt.Text = self.ax.text(
+            1, 1.05, displayed_values, fontsize=self.text_font_size, transform=self.ax.transAxes, ha='right')
+        self.ax.text(
+            0, 1.05, displayed_labels, fontsize=self.text_font_size, transform=self.ax.transAxes, ha='left')
 
     def init(self):
         self.line.set_ydata(self.ydata)
+        if self.show_rolling_mean:
+            self.rolling_mean_line.set_ydata(self.rolling_mean)
+        if self.show_reading_text:
+            self.current_value_text.set_text('NaN\nNaN\nNaN (NaN)' if self.show_rolling_mean else 'NaN')
         return self.line,
 
     def update(self, y):
@@ -97,11 +156,56 @@ class ScopeFigure:
         new_min = np.max([0, np.min(self.ydata) - delta])
         new_max = np.max(self.ydata) + delta
         current_min, current_max = self.ax.get_ylim()
-        if (np.abs((new_min - current_min) / (current_min)) > 0.12) or (
-                np.abs((new_max - current_max) / (current_max)) > 0.12):
+
+        if (np.abs((new_min - current_min) / current_min) > 0.12) \
+                or (np.abs((new_max - current_max) / current_max) > 0.12):
             self.ax.set_ylim(np.max([0.01, np.min(self.ydata) - delta]), np.max(self.ydata) + delta)
+
         self.line.set_ydata(self.ydata)
+
+        if self.show_rolling_mean:
+            new_rm_value = self.update_rolling_mean()
+            self.rolling_mean_line.set_ydata(self.rolling_mean)
+
+        if self.show_reading_text:
+            text = self._get_text_value(y)
+            if self.show_rolling_mean:
+                text = f'{text}\n {self._get_text_value(new_rm_value)}'
+                measured_stdev = self.get_new_rolling_stdev_val()
+                expected_stdev = np.sqrt(new_rm_value)
+                text = f'{text}\n {self._get_text_value(measured_stdev)} ({self._get_text_value(expected_stdev)})'
+            self.current_value_text.set_text(text)
+
         return self.line,
+
+    def get_new_rolling_mean_val(self):
+        values_of_interest = list(self.ydata)[len(self.ydata) - 2 * self.half_window_size:
+                                              len(self.ydata)]
+        return np.mean(values_of_interest)
+
+    def get_new_rolling_stdev_val(self):
+        values_of_interest = list(self.ydata)[len(self.ydata) - 2 * self.half_window_size:
+                                              len(self.ydata)]
+        return np.std(values_of_interest)
+
+    def update_rolling_mean(self):
+        rm = collections.deque(self.rolling_mean[self.half_window_size: len(self.ydata) - self.half_window_size])
+        rm.popleft()
+        new_rm_value = self.get_new_rolling_mean_val()
+        rm.append(new_rm_value)
+        self.rolling_mean[self.half_window_size: len(self.ydata) - self.half_window_size] = list(rm)
+        return new_rm_value
+
+    def get_rolling_mean(self):
+        rolling_mean = get_rolling_mean(self.ydata, self.rolling_mean_window_size)
+        rolling_mean[:self.half_window_size] = np.nan
+        rolling_mean[len(self.ydata) - self.half_window_size:] = np.nan
+        return rolling_mean
+
+    @staticmethod
+    def _get_text_value(value: float):
+        rounded_value = np.around(value, 1)
+        return f'{rounded_value:,}'
 
 
 class MainApplicationView:
@@ -109,7 +213,8 @@ class MainApplicationView:
         frame = Tk.Frame(main_frame)
         frame.pack(side=Tk.LEFT, fill=Tk.BOTH, expand=True)
 
-        self.scope_view = ScopeFigure(args.scope_width)
+        rolling_mean_window_size = np.ceil(args.rolling_mean_window / args.animation_update_interval)
+        self.scope_view = ScopeFigure(args.scope_width, int(rolling_mean_window_size))
         self.sidepanel = SidePanel(main_frame)
 
         self.canvas = FigureCanvasTkAgg(self.scope_view.fig, master=frame)
@@ -196,7 +301,8 @@ def build_data_model():
 
 
 def run_console():
-    view = ScopeFigure(args.scope_width)
+    rolling_mean_window_size = np.ceil(args.rolling_mean_window / args.animation_update_interval)
+    view = ScopeFigure(args.scope_width, int(rolling_mean_window_size))
     model = build_data_model()
     model.start()
     ani = animation.FuncAnimation(
