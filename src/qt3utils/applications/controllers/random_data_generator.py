933d07084a729232fe14dbc2eecd10ee1f104c26
G Adam Cox <gadamc@gmail.com>
2024-01-31 12:55:14 -0800

    Adds random spectrometer class.
    Fixes bugs found in controller.
    Random Spectrometer works so far.

--- src/qt3utils/applications/controllers/random_data_generator.py
+++ src/qt3utils/applications/controllers/random_data_generator.py
@@ -137,6 +137,161 @@ class QT3ScanRandomDataController(QT3ScopeRandomDataController):
         return self.data_generator.sample_count_rate(data_counts)
 
 
+class QT3ScanRandomSpectrometerDataController:
+    """
+    Implements qt3utils.applications.qt3scan.interface.QT3ScanSpectrometerDAQControllerInterface
+    """
+    def __init__(self, logger_level: int):
+        self.logger = logging.getLogger(__name__)
+        self.logger.setLevel(logger_level)
+
+        self.spectrometer = self.RandomSpectometer()
+
+        self.last_config_dict = {}
+
+        self.last_measured_spectrum = None
+        self.last_wavelength_array = None
+
+    class RandomSpectometer:
+        def __init__(self):
+            self.exposure_time = 500 # milliseconds
+            self.experiment_name = "Experiment3"
+            self.num_wavelength_bins = 250
+            self.wave_start = 600
+            self.wave_end = 850
+            self.num_frames = 1
+            self.center_wavelength = 700  # nm
+            self.temperature_sensor_setpoint = -70  # grad
+
+        def acquire_step_and_glue(self) -> Tuple[np.ndarray, np.ndarray]:
+            wavelengths = np.linspace(self.wave_start, self.wave_end, self.num_wavelength_bins)
+            spectrum = np.random.random(self.num_wavelength_bins)
+            return spectrum, wavelengths
+
+    @property
+    def clock_rate(self) -> float:
+        try:
+            _t = self.spectrometer.exposure_time / 1000.0  # converts from milliseconds to seconds.
+        except Exception as e:
+            self.logger.error(e)
+            _t = 2  # TODO: better default behavior. Should this be -1? 1? or should Spectrometer be changed.
+
+        return 1.0 / _t
+
+    def start(self) -> None:
+        # this function should take data for the current settings of the spectromter.
+        # All data acquistion should occur here.
+        self.logger.debug('calling QT3ScanRandomSpectrometerDataController start')
+        # nothing to be done
+
+    def stop(self) -> None:
+        """
+        Implementations should do necessary steps to stop acquiring data.
+        """
+        # if there is a way to interrupt data acquistion, do that here. Otherwise, do nothing
+        self.logger.debug('calling QT3ScanRandomSpectrometerDataController stop')
+
+    def close(self) -> None:
+        self.logger.debug('calling QT3ScanRandomSpectrometerDataController close')
+
+    def sample_spectrum(self) -> Tuple[np.ndarray, np.ndarray]:
+        self.last_measured_spectrum, self.last_wavelength_array = (
+            self.spectrometer.acquire_step_and_glue()
+        )
+        self.logger.debug(f'acquired spectrum from {self.last_wavelength_array[0]}'
+                          f'to {self.last_wavelength_array[-1]} nm')
+        return self.last_measured_spectrum, self.last_wavelength_array
+
+    def configure(self, config_dict: dict) -> None:
+        """
+        This method is used to configure the spectrometer with the provided settings.
+        """
+        self.logger.debug("QT3ScanRandomSpectrometerDataController.configure called")
+        self.last_config_dict.update(config_dict)
+
+        self.spectrometer.experiment_name = config_dict.get('experiment_name', self.spectrometer.experiment_name)
+        self.spectrometer.center_wavelength = config_dict.get('center_wavelength', self.spectrometer.center_wavelength)
+        self.spectrometer.exposure_time = config_dict.get('exposure_time', self.spectrometer.exposure_time)
+        self.spectrometer.temperature_sensor_setpoint = config_dict.get('temperature_sensor_setpoint', self.spectrometer.temperature_sensor_setpoint)
+        self.spectrometer.num_frames = config_dict.get('num_frames', self.spectrometer.num_frames)
+        self.spectrometer.num_wavelength_bins = config_dict.get('num_wavelength_bins', self.spectrometer.num_wavelength_bins)
+
+        self.spectrometer.wave_start = config_dict.get('wave_start', self.spectrometer.wave_start)
+        self.spectrometer.wave_end = config_dict.get('wave_end', self.spectrometer.wave_end)
+
+    def configure_view(self, gui_root: tk.Toplevel) -> None:
+        """
+        This method launches a GUI window to configure the data controller.
+        """
+
+        config_win = tk.Toplevel(gui_root)
+        config_win.grab_set()
+        config_win.title('Princeton Spectrometer Settings')
+
+        # TODO: change all of the StringVar to DoubleVar and remove casting above
+        row = 0
+        tk.Label(config_win, text="Experiment Name)").grid(row=row, column=0, padx=10)
+        experiment_name_var = tk.StringVar(value=str(self.spectrometer.experiment_name))
+        tk.Entry(config_win, textvariable=experiment_name_var).grid(row=row, column=1)
+
+        row += 1
+        tk.Label(config_win, text="Exposure Time (ms)").grid(row=row, column=0, padx=10)
+        exposure_time_var = tk.IntVar(value=str(self.spectrometer.exposure_time))
+        tk.Entry(config_win, textvariable=exposure_time_var).grid(row=row, column=1)
+
+        row += 1
+        tk.Label(config_win, text="Center Wavelength (nm)").grid(row=row, column=0, padx=10)
+        center_wavelength_var = tk.IntVar(value=str(self.spectrometer.center_wavelength))
+        tk.Entry(config_win, textvariable=center_wavelength_var).grid(row=row, column=1)
+
+        row += 1
+        tk.Label(config_win, text="Temperature Sensor Setpoint (°C)").grid(row=row, column=0, padx=10)
+        temperature_sensor_setpoint_var = tk.IntVar(value=str(self.spectrometer.temperature_sensor_setpoint))
+        tk.Entry(config_win, textvariable=temperature_sensor_setpoint_var).grid(row=row, column=1)
+
+        row += 1
+        tk.Label(config_win, text="Num Wavelength Bins").grid(row=row, column=0, padx=10)
+        num_wavelength_bins_var = tk.IntVar(value=self.spectrometer.num_wavelength_bins)
+        tk.Entry(config_win, textvariable=num_wavelength_bins_var).grid(row=row, column=1)
+
+        row += 1
+        tk.Label(config_win, text="Wavelength Start (nm)").grid(row=row, column=0, padx=10)
+        wave_start_var = tk.IntVar(value=str(self.spectrometer.wave_start))
+        tk.Entry(config_win, textvariable=wave_start_var).grid(row=row, column=1)
+
+        row += 1
+        tk.Label(config_win, text="Wavelength End (nm)").grid(row=row, column=0, padx=10)
+        wave_end_var = tk.IntVar(value=str(self.spectrometer.wave_end))
+        tk.Entry(config_win, textvariable=wave_end_var).grid(row=row, column=1)
+
+        # Pack variables into a dictionary to pass to the _set_from_gui method
+        gui_info = {
+            'experiment_name': experiment_name_var,
+            'exposure_time': exposure_time_var,
+            'center_wavelength': center_wavelength_var,
+            'temperature_sensor_setpoint': temperature_sensor_setpoint_var,
+            'num_wavelength_bins': num_wavelength_bins_var,
+            'wave_start': wave_start_var,
+            'wave_end': wave_end_var
+        }
+
+        row += 1
+        tk.Button(config_win, text='Set', command=lambda: self._set_from_gui(gui_info)).grid(row=row, column=0)
+        tk.Button(config_win, text='Close', command=config_win.destroy).grid(row=row, column=1)
+
+    def _set_from_gui(self, gui_vars: dict) -> None:
+        """
+        Sets the spectrometer configuration from the GUI.
+        """
+        config_dict = {k:v.get() if v.get() not in ['None', ''] else None for k, v in gui_vars.items()}  # code to handle the edge case where there are "None" value
+        self.logger.info(config_dict)
+        self.configure(config_dict)
+
+    def print_config(self) -> None:
+        print("Princeton Spectrometer config")
+        print(self.last_config_dict)  # we dont' use the logger because we want to be sure this is printed to stdout
+
+
 class QT3ScanDummyPositionController:
     """
     Implements the qt3utils.applications.qt3scan.interface.QT3ScanPositionControllerInterface for a dummy position controller.
